be6fe0f4ec4f7dc12ef6cb7989520507f3521480
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-02-08 12:25:43 +0700

    Add expect module and update login test

--- packages/server/src/index.login.spec.ts
+++ packages/server/src/index.login.spec.ts
@@ -1,20 +1,19 @@
-import { describe, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { buildHTTPExecutor } from '@graphql-tools/executor-http'
 import { yoga } from "./index"
 import { parse } from "graphql";
 
 describe("Login", () => {
     vi.mock('@tsndr/cloudflare-worker-jwt', async () => ({}))
-    vi.mock('./utils/storage', async () => ({
-        storage: () => ({
-            put: () => {},
-            get: () => {},
-        })
-    }))
-    const exec = buildHTTPExecutor({ fetch: yoga.fetch });
-
 
     it("should return 404 when user not found", async () => {
+        vi.mock('./utils/storage', async () => ({
+            storage: () => ({
+                put: () => {},
+                get: () => {},
+            })
+        }))
+        const exec = buildHTTPExecutor({ fetch: yoga.fetch });
         const result = await exec({
             document: parse(`#graphql
                 mutation {
@@ -26,6 +25,28 @@ describe("Login", () => {
                 }
             `) as any
         })
+
+        expect(result).toMatchInlineSnapshot(`
+          {
+            "data": {
+              "login": null,
+            },
+            "errors": [
+              {
+                "locations": [
+                  {
+                    "column": 3,
+                    "line": 2,
+                  },
+                ],
+                "message": "User not found",
+                "path": [
+                  "login",
+                ],
+              },
+            ],
+          }
+        `)
     })
 
     it("should return 401 when password incorrect", () => {})
