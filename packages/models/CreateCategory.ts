301b57c6b00d6d039942a63409ff12bd66bd3299
gongbaodd@ProArt <gongbaodd@qq.com>
2024-02-16 19:24:10 +0700

    fix item

--- packages/models/CreateCategory.ts
+++ packages/models/CreateCategory.ts
@@ -1,47 +1,48 @@
 import { Type } from "./Categories";
 
-let category: Pick<Type, "name"> | null = null
-let listeners: Function[] = []
+let category: Pick<Type, "name"> | null = null;
+let listeners: Function[] = [];
 
 export const CreateCategoryStore = {
-    getSnapshot() {
-        return category
-    },
-    subscribe(listener: Function) {
-        listeners.push(listener)
+  getSnapshot() {
+    return category;
+  },
+  subscribe(listener: Function) {
+    listeners.push(listener);
 
-        return () => {
-            listeners = listeners.filter(l => l === listener)
-        }
-    },
-    setName,
-    clear,
-    isValid,
-}
+    return () => {
+      listeners = listeners.filter((l) => l === listener);
+    };
+  },
+  setName,
+  clear,
+  isValid,
+};
 
 function emitChange() {
-    listeners.forEach(l => l())
+  listeners.forEach((l) => l());
 }
 
 function setName(name: Type["name"]) {
-    if (!category) category = {} as any
-
-    category!.name = name
-    emitChange()
+  category = {
+    ...(category ?? ({} as any)),
+    name,
+  };
+  emitChange();
 }
 
 function clear() {
-    category = null
-    emitChange()
+  category = null;
+  emitChange();
 }
 
 function isValid() {
-    if (!category) return false
-    const keys = ["name"] satisfies (keyof typeof category)[]
+  if (!category) return false;
+  const keys = ["name"] satisfies (keyof typeof category)[];
 
-    for (const key of keys) {
-        if (!category[key]) return false
-    }
+  for (const key of keys) {
+    if (!category[key]) return false;
+  }
 
-    return true
+  return true;
 }
--- packages/models/CreateItem.ts
+++ packages/models/CreateItem.ts
@@ -1,67 +1,73 @@
 import { Item } from "./Item";
 
 let item: Pick<Item, "name" | "price" | "type"> | null = null;
-let listeners: Function[] = []
+let listeners: Function[] = [];
 
 export const CreateItemStore = {
-    getSnapshot() {
-        return item
-    },
-    subscribe(listener: Function) {
-        listeners.push(listener)
-        return () => {
-            listeners = listeners.filter(l => l === listener)
-        }
-    },
-    setName,
-    setType,
-    setPrice,
-    isValid,
-    clear,
-}
+  getSnapshot() {
+    return item;
+  },
+  subscribe(listener: Function) {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l === listener);
+    };
+  },
+  setName,
+  setType,
+  setPrice,
+  isValid,
+  clear,
+};
 
 function emitChange() {
-    listeners.forEach(l => l())
+  listeners.forEach((l) => l());
 }
 
 function setName(name: Item["name"]) {
-    if (!item) item = {} as any
-    item!.name = name
-    emitChange()
+  item = {
+    ...(item ?? ({} as any)),
+    name,
+  };
+  emitChange();
 }
 
 function setType(type: Item["type"]) {
-    if (!item) item = {} as any
-    item!.type = type 
-    emitChange()
+  item = {
+    ...(item ?? ({} as any)),
+    type,
+  };
+
+  emitChange();
 }
 
 function setPrice(price: Item["price"]) {
-    if (!item) item = {} as any
-    item!.price = price
-    emitChange()
+  item = {
+    ...(item ?? ({} as any)),
+    price,
+  };
+  emitChange();
 }
 
 function isValid() {
-    if (item) {
-        const keys = ["name", "type", "price"] satisfies (keyof typeof item)[]
+  if (item) {
+    const keys = ["name", "type", "price"] satisfies (keyof typeof item)[];
 
-        for (const key of keys) {
-            if (!item[key]) return false
-        }
-
-        if (!item["type"]["id"]) {
-            return false
-        }
-
-        return true
+    for (const key of keys) {
+      if (!item[key]) return false;
     }
 
-    return false
+    if (!item["type"]["id"]) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
 }
 
 function clear() {
-    item = null
-    emitChange()
+  item = null;
+  emitChange();
 }
-
